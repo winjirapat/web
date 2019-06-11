e67d7e826bb11fc41b5985695583d72ea39ee8c1
winjirapat <wim22_2531@hotmail.com>
2019-06-11 16:27:27 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './Home.jpg';
+import './App.css';
+
+
+
+
+function App() {
+  return (
+    
+    <div >
+     
+        <img  style={{width:"100%",height:"100%"}}  src={logo}  alt="logo" />
+       
+    
+    </div>
+    
+  );
+}
+
+export default App;
